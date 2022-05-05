e79bc5457a28a009c4bdb78d24e1440c174596ae
Christian Raoulis <craoulis@googlemail.com>
2022-05-05 10:07:54 +0200

    fix transparency of glass and ice blocks

--- src/main/java/com/manasmods/vanilla_plus/Vanilla_PlusClient.java
+++ src/main/java/com/manasmods/vanilla_plus/Vanilla_PlusClient.java
@@ -4,6 +4,7 @@ import com.manasmods.vanilla_plus.block.Vanilla_PlusBlocks;
 import com.manasmods.vanilla_plus.client.handler.Vanilla_PlusColorHandler;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class Vanilla_PlusClient extends Vanilla_PlusCommon {
@@ -15,9 +16,23 @@ public class Vanilla_PlusClient extends Vanilla_PlusCommon {
 
     @Override
     public void clientInit() {
-        ItemBlockRenderTypes.setRenderLayer(Vanilla_PlusBlocks.GRASS_BLOCK_STAIRS, RenderType.cutoutMipped());
-        ItemBlockRenderTypes.setRenderLayer(Vanilla_PlusBlocks.GRASS_BLOCK_SLAB, RenderType.cutoutMipped());
-        ItemBlockRenderTypes.setRenderLayer(Vanilla_PlusBlocks.GLASS_STAIRS, RenderType.cutoutMipped());
-        ItemBlockRenderTypes.setRenderLayer(Vanilla_PlusBlocks.GLASS_SLAB, RenderType.cutoutMipped());
+        cutoutMipped(Vanilla_PlusBlocks.GRASS_BLOCK_STAIRS);
+        cutoutMipped(Vanilla_PlusBlocks.GRASS_BLOCK_SLAB);
+        cutout(Vanilla_PlusBlocks.GLASS_STAIRS);
+        cutout(Vanilla_PlusBlocks.GLASS_SLAB);
+        translucent(Vanilla_PlusBlocks.ICE_STAIRS);
+        translucent(Vanilla_PlusBlocks.ICE_SLAB);
+    }
+
+    private void cutoutMipped(Block block) {
+        ItemBlockRenderTypes.setRenderLayer(block, RenderType.cutoutMipped());
+    }
+
+    private void cutout(Block block) {
+        ItemBlockRenderTypes.setRenderLayer(block, RenderType.cutout());
+    }
+
+    private void translucent(Block block) {
+        ItemBlockRenderTypes.setRenderLayer(block, RenderType.translucent());
     }
 }
