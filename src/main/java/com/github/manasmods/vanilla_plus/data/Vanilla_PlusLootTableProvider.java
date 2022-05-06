f87fe2b928a5d930a3fb50f308c3f55437d2b9a5
Nieadni <diablomaster761@gmail.com>
2022-05-06 22:27:57 +0100

    I hate java

--- src/main/java/com/github/manasmods/vanilla_plus/data/Vanilla_PlusLootTableProvider.java
+++ src/main/java/com/github/manasmods/vanilla_plus/data/Vanilla_PlusLootTableProvider.java
@@ -5,11 +5,14 @@ import com.mojang.datafixers.util.Pair;
 import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.ValidationContext;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSet;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -25,6 +28,11 @@ public class Vanilla_PlusLootTableProvider extends LootTableProvider {
     @Override
     protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootContextParamSet>> getTables() {
         return ImmutableList.of(Pair.of(Vanilla_PlusBlockLoot::new, LootContextParamSets.BLOCK));
+    }
+
+    @Override
+    protected void validate(Map<ResourceLocation, LootTable> map, ValidationContext validationtracker) {
+        map.forEach((resourceLocation, lootTable) -> LootTables.validate(validationtracker, resourceLocation, lootTable));
     }
 }
 
