e4618691f204490578f6b7e963012eb5f8f989e7
Nieadni <diablomaster761@gmail.com>
2022-05-08 12:02:57 +0100

    ¬ Attempted to add Fletching Table gui ¬ Attempted to add block loot

--- src/main/java/com/github/manasmods/vanilla_plus/block/entity/FletchingTableBlockEntity.java
+++ src/main/java/com/github/manasmods/vanilla_plus/block/entity/FletchingTableBlockEntity.java
@@ -2,18 +2,51 @@ package com.github.manasmods.vanilla_plus.block.entity;
 
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.Clearable;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.network.NetworkHooks;
 
-public class FletchingTableBlockEntity extends BlockEntity implements Clearable {
+import javax.annotation.Nullable;
+
+public class FletchingTableBlockEntity extends BlockEntity {
     public FletchingTableBlockEntity(BlockPos pWorldPosition, BlockState pBlockState) {
         super(Vanilla_PlusBlockEntities.FLETCHING_TABLE_BLOCK_ENTITY, pWorldPosition, pBlockState);
 
     }
 
     @Override
-    public void clearContent() {
+    public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
+        if (pState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = pLevel.getBlockEntity(pPos);
+            if (blockEntity instanceof FletchingTableBlockEntity) {
+                ((FletchingTableBlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
+    }
 
+    private void drops() {
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos,
+                                 Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if (!pLevel.isClientSide()) {
+            BlockEntity entity = pLevel.getBlockEntity(pPos);
+            if (entity instanceof FletchingTableBlockEntity) {
+                NetworkHooks.openGui(((ServerPlayer) pPlayer), (MenuProvider) entity, pPos);
+            } else {
+                throw new IllegalStateException("Our Container provider is missing!");
+            }
+        }
+
+        return InteractionResult.sidedSuccess(pLevel.isClientSide());
     }
 }
