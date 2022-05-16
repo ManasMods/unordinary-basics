b02c9e82b1ad09f4b41064f40f172635d1361e36
Christian Raoulis <craoulis@googlemail.com>
2022-05-16 14:52:36 +0200

    refactor all tanslation key in en_us.json
    Signed-off-by: Christian Raoulis <craoulis@googlemail.com>

--- src/main/java/com/github/manasmods/unordinary_basics/item/Unordinary_BasicsCreativeTab.java
+++ src/main/java/com/github/manasmods/unordinary_basics/item/Unordinary_BasicsCreativeTab.java
@@ -1,12 +1,13 @@
 package com.github.manasmods.unordinary_basics.item;
 
+import com.github.manasmods.unordinary_basics.Unordinary_Basics;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
 public class Unordinary_BasicsCreativeTab {
 
-    public static final CreativeModeTab BLOCKS = new CreativeModeTab("vanilla_plustab") {
+    public static final CreativeModeTab BLOCKS = new CreativeModeTab(Unordinary_Basics.MOD_ID + ".tab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Items.BRICKS);
