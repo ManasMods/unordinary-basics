2c6d3be759c2b1fcf00618e471071af0d6ba1c04
Christian Raoulis <craoulis@googlemail.com>
2022-06-25 01:23:46 +0200

    remove unused method and shadows
    Signed-off-by: Christian Raoulis <craoulis@googlemail.com>

--- src/main/java/com/github/manasmods/unordinary_basics/core/MixinAbstractHorse.java
+++ src/main/java/com/github/manasmods/unordinary_basics/core/MixinAbstractHorse.java
@@ -1,13 +1,5 @@
 package com.github.manasmods.unordinary_basics.core;
 
-import java.awt.event.ContainerListener;
-
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -18,60 +10,63 @@ import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import javax.annotation.Nullable;
+import java.awt.event.ContainerListener;
 
 @Mixin(AbstractHorse.class)
-public abstract class MixinAbstractHorse extends Animal implements ContainerListener, PlayerRideableJumping, Saddleable
-{
-
-    protected MixinAbstractHorse(EntityType<? extends Animal> p_27557_, Level p_27558_)
-    {
+public abstract class MixinAbstractHorse extends Animal implements ContainerListener, PlayerRideableJumping, Saddleable {
+    protected MixinAbstractHorse(EntityType<? extends Animal> p_27557_, Level p_27558_) {
         super(p_27557_, p_27558_);
     }
-    
+
     @Shadow
     protected int gallopSoundCounter;
-    
+
     @Shadow
     protected float playerJumpPendingScale;
-    
+
     @Shadow
     private boolean allowStandSliding;
-    
-    @Shadow
-    private float standAnimO;
-    
+
     @Override
     public boolean rideableUnderWater() {
         return true;
     }
-    
+
     @Shadow
     public abstract boolean isStanding();
-    
-    @Shadow
-    public abstract boolean isJumping();
-    
+
     @Shadow
     public abstract double getCustomJump();
-    
+
     @Shadow
     public abstract void setIsJumping(boolean pJumping);
-    
+
+    @Shadow
+    @Nullable
+    public abstract Entity getControllingPassenger();
+
     @Inject(method = "travel(Lnet/minecraft/world/phys/Vec3;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/horse/AbstractHorse;getSpeed()F"))
     public void travel(Vec3 pTravelVector, CallbackInfo info) {
-        LivingEntity livingentity = (LivingEntity)this.getControllingPassenger();
+        LivingEntity livingentity = (LivingEntity) getControllingPassenger();
         float f1 = livingentity.zza;
         if (f1 <= 0.0F) {
-           f1 *= 0.25F;
-           this.gallopSoundCounter = 0;
+            f1 *= 0.25F;
+            this.gallopSoundCounter = 0;
         }
 
         if (this.onGround && this.playerJumpPendingScale == 0.0F && this.isStanding() && !this.allowStandSliding) {
-           f1 = 0.0F;
+            f1 = 0.0F;
         }
-        
-        if(this.isUnderWater() && this.playerJumpPendingScale > 0.0F && !this.onGround) {
-            double d0 = this.getCustomJump() * (double)this.playerJumpPendingScale * (double)this.getBlockJumpFactor();
+
+        if (this.isUnderWater() && this.playerJumpPendingScale > 0.0F && !this.onGround) {
+            double d0 = this.getCustomJump() * (double) this.playerJumpPendingScale * (double) this.getBlockJumpFactor();
             double d1 = d0 + this.getJumpBoostPower();
             Vec3 vec3 = this.getDeltaMovement();
             this.setDeltaMovement(vec3.x, d1, vec3.z);
@@ -79,28 +74,28 @@ public abstract class MixinAbstractHorse extends Animal implements ContainerList
             this.hasImpulse = true;
             net.minecraftforge.common.ForgeHooks.onLivingJump(this);
             if (f1 > 0.0F) {
-               float f2 = Mth.sin(this.getYRot() * ((float)Math.PI / 180F));
-               float f3 = Mth.cos(this.getYRot() * ((float)Math.PI / 180F));
-               this.setDeltaMovement(this.getDeltaMovement().add((double)(-0.4F * f2 * this.playerJumpPendingScale), 0.0D, (double)(0.4F * f3 * this.playerJumpPendingScale)));
+                float f2 = Mth.sin(this.getYRot() * ((float) Math.PI / 180F));
+                float f3 = Mth.cos(this.getYRot() * ((float) Math.PI / 180F));
+                this.setDeltaMovement(this.getDeltaMovement().add((double) (-0.4F * f2 * this.playerJumpPendingScale), 0.0D, (double) (0.4F * f3 * this.playerJumpPendingScale)));
             }
-            
+
             this.playerJumpPendingScale = 0.0F;
         }
-     }
-    
+    }
+
     @Inject(method = "positionRider(Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "JUMP", ordinal = 0))
     public void positionRider(Entity pPassenger, CallbackInfo info) {
         double d0 = this.getY() + this.getPassengersRidingOffset() + pPassenger.getMyRidingOffset();
         int i = this.getPassengers().indexOf(pPassenger);
-        
-        if(i > 0) {
-            float f3 = Mth.sin(this.yBodyRot * ((float)Math.PI / 180F));
-            float f = Mth.cos(this.yBodyRot * ((float)Math.PI / 180F));
-            
-            pPassenger.setPos(this.getX()+ (double)(0.5 * f3), d0, this.getZ() - (double)(0.5 * f));
+
+        if (i > 0) {
+            float f3 = Mth.sin(this.yBodyRot * ((float) Math.PI / 180F));
+            float f = Mth.cos(this.yBodyRot * ((float) Math.PI / 180F));
+
+            pPassenger.setPos(this.getX() + (double) (0.5 * f3), d0, this.getZ() - (double) (0.5 * f));
         }
-     }
-    
+    }
+
     @Override
     public boolean canAddPassenger(Entity other) {
         return this.getPassengers().size() < 2;
--- src/main/java/com/github/manasmods/unordinary_basics/core/MixinHorse.java
+++ src/main/java/com/github/manasmods/unordinary_basics/core/MixinHorse.java
@@ -1,71 +1,14 @@
 package com.github.manasmods.unordinary_basics.core;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.At;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.entity.animal.horse.Horse;
-import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Horse.class)
-public abstract class MixinHorse extends AbstractHorse
-{
-
-    protected MixinHorse(EntityType<? extends AbstractHorse> p_30531_, Level p_30532_)
-    {
-        super(p_30531_, p_30532_);
-    }
-    
-    /*@Inject(method = "mobInteract(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult", 
-            at = @At(value = "RETURN", ordinal = 1), cancellable = true)
-    public void mobInteract(Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResult> cir) {
-        cir.cancel();
-    }*/
-    
-    /*@Overwrite
-    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
-        ItemStack itemstack = pPlayer.getItemInHand(pHand);
-        if (!this.isBaby()) {
-           if (this.isTamed() && pPlayer.isSecondaryUseActive()) {
-              this.openInventory(pPlayer);
-              return InteractionResult.sidedSuccess(this.level.isClientSide);
-           }
-        }
-
-        if (!itemstack.isEmpty()) {
-           if (this.isFood(itemstack)) {
-              return this.fedFood(pPlayer, itemstack);
-           }
-
-           InteractionResult interactionresult = itemstack.interactLivingEntity(pPlayer, this, pHand);
-           if (interactionresult.consumesAction()) {
-              return interactionresult;
-           }
-
-           if (!this.isTamed()) {
-              this.makeMad();
-              return InteractionResult.sidedSuccess(this.level.isClientSide);
-           }
-
-           boolean flag = !this.isBaby() && !this.isSaddled() && itemstack.is(Items.SADDLE);
-           if (this.isArmor(itemstack) || flag) {
-              this.openInventory(pPlayer);
-              return InteractionResult.sidedSuccess(this.level.isClientSide);
-           }
-        }
-
-        if (this.isBaby() || this.getPassengers().size() >= 2) {
-           return super.mobInteract(pPlayer, pHand);
-        } else {
-           this.doPlayerRide(pPlayer);
-           return InteractionResult.sidedSuccess(this.level.isClientSide);
-        }
-     }*/
-    
+public class MixinHorse {
     @Redirect(method = "mobInteract", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/horse/Horse;isVehicle()Z"))
     public boolean onMobInteract(Horse instance) {
         return false;
     }
-
 }
