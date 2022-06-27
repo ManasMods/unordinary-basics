65aecc833f0dabb4e5b5e91dcaaceb03bb1502f3
Contrabass26 <jsedwards89@hotmail.co.uk>
2022-06-27 13:54:17 +0100

    Full netherite reduces fire damage

--- src/main/java/com/github/manasmods/unordinary_basics/Unordinary_BasicsEvents.java
+++ src/main/java/com/github/manasmods/unordinary_basics/Unordinary_BasicsEvents.java
@@ -1,8 +1,10 @@
 package com.github.manasmods.unordinary_basics;
 
 import com.github.manasmods.unordinary_basics.utils.MixinLadderHelper;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.world.BlockEvent;
@@ -11,18 +13,23 @@ import net.minecraftforge.fml.common.Mod;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.StreamSupport;
 
 @Mod.EventBusSubscriber(modid = Unordinary_Basics.MOD_ID)
 public class Unordinary_BasicsEvents {
 
     private static final Set<Item> CHAINMAIL_ARMOR;
+    private static final Set<Item> NETHERITE_ARMOR;
     static {
         CHAINMAIL_ARMOR = new HashSet<>();
         CHAINMAIL_ARMOR.add(Items.CHAINMAIL_HELMET);
         CHAINMAIL_ARMOR.add(Items.CHAINMAIL_CHESTPLATE);
         CHAINMAIL_ARMOR.add(Items.CHAINMAIL_LEGGINGS);
         CHAINMAIL_ARMOR.add(Items.CHAINMAIL_BOOTS);
+        NETHERITE_ARMOR = new HashSet<>();
+        NETHERITE_ARMOR.add(Items.NETHERITE_HELMET);
+        NETHERITE_ARMOR.add(Items.NETHERITE_CHESTPLATE);
+        NETHERITE_ARMOR.add(Items.NETHERITE_LEGGINGS);
+        NETHERITE_ARMOR.add(Items.NETHERITE_BOOTS);
     }
 
     @SubscribeEvent
@@ -33,10 +40,21 @@ public class Unordinary_BasicsEvents {
     @SubscribeEvent
     public static void onLivingEntityHurt(LivingHurtEvent event) {
         if (event.getEntityLiving() instanceof Player player) {
-            boolean allChainmail = StreamSupport.stream(player.getArmorSlots().spliterator(), false).allMatch(o -> CHAINMAIL_ARMOR.contains(o.getItem()));
-            if (allChainmail) {
+            if (fullArmorSet(player, CHAINMAIL_ARMOR) && event.getSource().msgId.equals("arrow")) {
                 event.setAmount(event.getAmount() * 0.75f);
             }
+            if (fullArmorSet(player, NETHERITE_ARMOR) && (event.getSource() == DamageSource.LAVA || event.getSource() == DamageSource.IN_FIRE || event.getSource() == DamageSource.ON_FIRE)) {
+                event.setAmount(event.getAmount() * 0.7f);
+            }
         }
+    }
+
+    private static boolean fullArmorSet(Player player, Set<Item> armorSet) {
+        for (ItemStack stack : player.getArmorSlots()) {
+            if (!armorSet.contains(stack.getItem())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
