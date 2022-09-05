87a45187e5f9559f35f4f5077d1ec29602e10447
Nieadni <diablomaster761@gmail.com>
2022-09-05 14:10:20 +0100

    Steve renders in new menu, and I made my own Grizzly Texture

--- src/main/java/com/github/manasmods/unordinary_basics/client/gui/Unordinary_BasicsInventoryScreen.java
+++ src/main/java/com/github/manasmods/unordinary_basics/client/gui/Unordinary_BasicsInventoryScreen.java
@@ -4,12 +4,19 @@ import com.github.manasmods.manascore.tab.annotation.ScreenForTab;
 import com.github.manasmods.unordinary_basics.Unordinary_Basics;
 import com.github.manasmods.unordinary_basics.menu.UBInventoryMenu;
 import com.github.manasmods.unordinary_basics.tab.Unordinary_BasicsInventoryTab;
+import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Quaternion;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -19,6 +26,8 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class Unordinary_BasicsInventoryScreen extends AbstractContainerScreen<UBInventoryMenu> {
 
     public static final ResourceLocation UB_INVENTORY_LOCATION = new ResourceLocation(Unordinary_Basics.MOD_ID, "textures/gui/ub_inventory.png");
+    private float xMouse;
+    private float yMouse;
 
     public Unordinary_BasicsInventoryScreen(UBInventoryMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
@@ -45,6 +54,55 @@ public class Unordinary_BasicsInventoryScreen extends AbstractContainerScreen<UB
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, UB_INVENTORY_LOCATION);
-        this.blit(poseStack, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
+        int i = this.leftPos;
+        int j = this.topPos;
+        this.blit(poseStack, i, j, 0, 0, this.imageWidth, this.imageHeight);
+        renderEntityInInventory(i + 71, j + 75, 30, (float)(i + 51) - this.xMouse, (float)(j + 75 - 50) - this.yMouse, this.minecraft.player);
+    }
+
+    public static void renderEntityInInventory(int pPosX, int pPosY, int pScale, float pMouseX, float pMouseY, LivingEntity pLivingEntity) {
+        float f = (float)Math.atan((double)(pMouseX / 40.0F));
+        float f1 = (float)Math.atan((double)(pMouseY / 40.0F));
+        PoseStack posestack = RenderSystem.getModelViewStack();
+        posestack.pushPose();
+        posestack.translate((double)pPosX, (double)pPosY, 1050.0D);
+        posestack.scale(1.0F, 1.0F, -1.0F);
+        RenderSystem.applyModelViewMatrix();
+        PoseStack posestack1 = new PoseStack();
+        posestack1.translate(0.0D, 0.0D, 1000.0D);
+        posestack1.scale((float)pScale, (float)pScale, (float)pScale);
+        Quaternion quaternion = Vector3f.ZP.rotationDegrees(180.0F);
+        Quaternion quaternion1 = Vector3f.XP.rotationDegrees(f1 * 20.0F);
+        quaternion.mul(quaternion1);
+        posestack1.mulPose(quaternion);
+        float f2 = pLivingEntity.yBodyRot;
+        float f3 = pLivingEntity.getYRot();
+        float f4 = pLivingEntity.getXRot();
+        float f5 = pLivingEntity.yHeadRotO;
+        float f6 = pLivingEntity.yHeadRot;
+        pLivingEntity.yBodyRot = 180.0F + f * 20.0F;
+        pLivingEntity.setYRot(180.0F + f * 40.0F);
+        pLivingEntity.setXRot(-f1 * 20.0F);
+        pLivingEntity.yHeadRot = pLivingEntity.getYRot();
+        pLivingEntity.yHeadRotO = pLivingEntity.getYRot();
+        Lighting.setupForEntityInInventory();
+        EntityRenderDispatcher entityrenderdispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        quaternion1.conj();
+        entityrenderdispatcher.overrideCameraOrientation(quaternion1);
+        entityrenderdispatcher.setRenderShadow(false);
+        MultiBufferSource.BufferSource multibuffersource$buffersource = Minecraft.getInstance().renderBuffers().bufferSource();
+        RenderSystem.runAsFancy(() -> {
+            entityrenderdispatcher.render(pLivingEntity, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F, posestack1, multibuffersource$buffersource, 15728880);
+        });
+        multibuffersource$buffersource.endBatch();
+        entityrenderdispatcher.setRenderShadow(true);
+        pLivingEntity.yBodyRot = f2;
+        pLivingEntity.setYRot(f3);
+        pLivingEntity.setXRot(f4);
+        pLivingEntity.yHeadRotO = f5;
+        pLivingEntity.yHeadRot = f6;
+        posestack.popPose();
+        RenderSystem.applyModelViewMatrix();
+        Lighting.setupFor3DItems();
     }
 }
