ccd23de8aae1fe48515de2a06bf0beeddc03483a
Nieadni <diablomaster761@gmail.com>
2022-09-21 22:17:13 +0100

    Mo' texture stuff

--- src/main/java/com/github/manasmods/unordinary_basics/data/Unordinary_BasicsItemModelProvider.java
+++ src/main/java/com/github/manasmods/unordinary_basics/data/Unordinary_BasicsItemModelProvider.java
@@ -3,6 +3,7 @@ package com.github.manasmods.unordinary_basics.data;
 import com.github.manasmods.manascore.data.gen.ItemModelProvider;
 import com.github.manasmods.unordinary_basics.Unordinary_Basics;
 import com.github.manasmods.unordinary_basics.item.Unordinary_BasicsItems;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 public class Unordinary_BasicsItemModelProvider extends ItemModelProvider {
@@ -30,5 +31,25 @@ public class Unordinary_BasicsItemModelProvider extends ItemModelProvider {
 
         singleTexture(Unordinary_BasicsItems.MUSIC_DISC_QUEEN);
 
+        singleTexture(Items.CHAINMAIL_HELMET);
+        singleTexture(Items.CHAINMAIL_CHESTPLATE);
+        singleTexture(Items.CHAINMAIL_LEGGINGS);
+        singleTexture(Items.CHAINMAIL_BOOTS);
+        singleTexture(Items.DIAMOND_HELMET);
+        singleTexture(Items.DIAMOND_CHESTPLATE);
+        singleTexture(Items.DIAMOND_LEGGINGS);
+        singleTexture(Items.DIAMOND_BOOTS);
+        singleTexture(Items.GOLDEN_HELMET);
+        singleTexture(Items.GOLDEN_CHESTPLATE);
+        singleTexture(Items.GOLDEN_LEGGINGS);
+        singleTexture(Items.GOLDEN_BOOTS);
+        singleTexture(Items.IRON_HELMET);
+        singleTexture(Items.IRON_CHESTPLATE);
+        singleTexture(Items.IRON_LEGGINGS);
+        singleTexture(Items.IRON_BOOTS);
+        singleTexture(Items.NETHERITE_HELMET);
+        singleTexture(Items.NETHERITE_CHESTPLATE);
+        singleTexture(Items.NETHERITE_LEGGINGS);
+        singleTexture(Items.NETHERITE_BOOTS);
     }
 }
