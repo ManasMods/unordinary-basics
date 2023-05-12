721f037438dfefd71f19c2703217a1b35541eda1
nexonerr <cavdarmete08@gmail.com>
2023-05-12 20:46:13 +0300

    Goats give goat milk now

--- src/main/java/com/github/manasmods/unordinary_basics/core/MixinGoat.java
+++ src/main/java/com/github/manasmods/unordinary_basics/core/MixinGoat.java
@@ -1,23 +1,32 @@
 package com.github.manasmods.unordinary_basics.core;
 
+import com.github.manasmods.unordinary_basics.item.Unordinary_BasicsItems;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.animal.goat.Goat;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Items;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Goat.class)
 public class MixinGoat {
-/*
-    @Inject(???)
-    public InteractionResult mobInteract(Player pPlayer, InteractionHand pHand) {
+
+    @Inject(method = "mobInteract(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/InteractionHand;)Lnet/minecraft/world/InteractionResult;", at = @At("HEAD"), cancellable = true)
+    public void mobInteract(Player pPlayer, InteractionHand pHand, CallbackInfoReturnable<InteractionResult> cir) {
+        cir.setReturnValue(InteractionResult.FAIL);
+        cir.cancel();
         ItemStack itemstack = pPlayer.getItemInHand(pHand);
         if (itemstack.is(Items.BUCKET)) {
             pPlayer.playSound(SoundEvents.COW_MILK, 1.0F, 1.0F);
             ItemStack itemstack1 = ItemUtils.createFilledResult(itemstack, pPlayer, Unordinary_BasicsItems.GOAT_MILK_BUCKET.getDefaultInstance());
             pPlayer.setItemInHand(pHand, itemstack1);
-            return InteractionResult.sidedSuccess(this.level.isClientSide);
-        } else {
-            return super.mobInteract(pPlayer, pHand);
+            cir.setReturnValue(InteractionResult.sidedSuccess(pPlayer.getLevel().isClientSide));
         }
     }
-*/
-// TODO: This ^^
 }
