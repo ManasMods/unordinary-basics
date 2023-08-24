bf37bace70267aa6dcf11315d466ef40aa2f3050
Nieadni <103650147+Nieadni@users.noreply.github.com>
2023-08-24 22:08:17 +0100

    Skull now impacts blocks and doesnt break them

--- src/main/java/com/github/manasmods/unordinary_basics/entity/NonExplosiveWitherSkull.java
+++ src/main/java/com/github/manasmods/unordinary_basics/entity/NonExplosiveWitherSkull.java
@@ -1,27 +1,26 @@
 package com.github.manasmods.unordinary_basics.entity;
 
 import com.github.manasmods.unordinary_basics.registry.UBEntityTypes;
-import net.minecraft.world.damagesource.IndirectEntityDamageSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
-import net.minecraft.world.level.Level;
-
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.IndirectEntityDamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.AbstractHurtingProjectile;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
-
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
 
 public class NonExplosiveWitherSkull extends AbstractHurtingProjectile {
     private static final EntityDataAccessor<Boolean> DATA_DANGEROUS = SynchedEntityData.defineId(NonExplosiveWitherSkull.class, EntityDataSerializers.BOOLEAN);
@@ -93,6 +92,19 @@ public class NonExplosiveWitherSkull extends AbstractHurtingProjectile {
             }
 
         }
+    }
+
+    /**
+     *  Called when this EntityFireball hits a block or entity.
+     **/
+    protected void onHit(HitResult pResult) {
+        super.onHit(pResult);
+        if (!this.level.isClientSide) {
+            Explosion.BlockInteraction explosion$blockinteraction = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.level, this.getOwner()) ? Explosion.BlockInteraction.NONE : Explosion.BlockInteraction.NONE;
+            this.level.explode(this, this.getX(), this.getY(), this.getZ(), 0.0F, false, explosion$blockinteraction);
+            this.discard();
+        }
+
     }
 
     /**
