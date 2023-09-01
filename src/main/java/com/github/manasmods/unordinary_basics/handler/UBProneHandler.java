492de3809a2f7a60e137a6fbaa4b83de0768891f
Minh <minheragon12345@gmail.com>
2023-09-01 18:01:22 +0700

    Fixed Proning

--- src/main/java/com/github/manasmods/unordinary_basics/handler/UBProneHandler.java
+++ src/main/java/com/github/manasmods/unordinary_basics/handler/UBProneHandler.java
@@ -1,10 +1,8 @@
 package com.github.manasmods.unordinary_basics.handler;
 
 import com.github.manasmods.unordinary_basics.Unordinary_Basics;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -16,9 +14,13 @@ public class UBProneHandler {
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         Player player = event.player;
         if (player.isCrouching()) {
-            if (!player.getLevel().getBlockState(player.getOnPos().above(2).relative(player.getDirection())).getCollisionShape(player.getLevel(), player.getOnPos().above(2).relative(player.getDirection())).isEmpty()
-                    && player.getPose() != Pose.SWIMMING
-                    && !player.getLevel().getBlockState(player.getOnPos().relative(player.getDirection())).getCollisionShape(player.getLevel(), player.getOnPos().relative(player.getDirection())).isEmpty() && player.getPose() != Pose.SWIMMING) {
+            boolean faceSolid = !player.getLevel().getBlockState(player.getOnPos().above(2).relative(player.getDirection()))
+                    .getCollisionShape(player.getLevel(), player.getOnPos().above(2).relative(player.getDirection())).isEmpty();
+            boolean kneeEmpty = player.getLevel().getBlockState(player.getOnPos().above().relative(player.getDirection()))
+                    .getCollisionShape(player.getLevel(), player.getOnPos().above().relative(player.getDirection())).isEmpty();
+            boolean solidGround = !player.getLevel().getBlockState(player.getOnPos().relative(player.getDirection()))
+                    .getCollisionShape(player.getLevel(), player.getOnPos().relative(player.getDirection())).isEmpty();
+            if (faceSolid && kneeEmpty && solidGround && player.getPose() != Pose.SWIMMING) {
                 player.setPos(player.getDirection().getStepX() / 2f + player.getX(), player.getY(), player.getDirection().getStepZ() / 2f + player.getZ());
                 player.setSwimming(true);
                 player.setPose(Pose.SWIMMING);
