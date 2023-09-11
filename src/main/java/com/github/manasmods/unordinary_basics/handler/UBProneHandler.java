b7197395669bb72f8aab26b49a4bca7374d732d3
Nieadni <103650147+Nieadni@users.noreply.github.com>
2023-09-11 14:05:20 +0100

    Fixed proning

--- src/main/java/com/github/manasmods/unordinary_basics/handler/UBProneHandler.java
+++ src/main/java/com/github/manasmods/unordinary_basics/handler/UBProneHandler.java
@@ -14,9 +14,13 @@ public class UBProneHandler {
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         Player player = event.player;
         if (player.isCrouching()) {
-            if (!player.getLevel().getBlockState(player.getOnPos().above(2).relative(player.getDirection())).getCollisionShape(player.getLevel(), player.getOnPos().above(2).relative(player.getDirection())).isEmpty()
-                    && player.getPose() != Pose.SWIMMING
-                    && !player.getLevel().getBlockState(player.getOnPos().relative(player.getDirection())).getCollisionShape(player.getLevel(), player.getOnPos().relative(player.getDirection())).isEmpty() && player.getPose() != Pose.SWIMMING) {
+            boolean faceSolid = !player.getLevel().getBlockState(player.getOnPos().above(2).relative(player.getDirection()))
+                    .getCollisionShape(player.getLevel(), player.getOnPos().above(2).relative(player.getDirection())).isEmpty();
+            boolean kneeEmpty = player.getLevel().getBlockState(player.getOnPos().above().relative(player.getDirection()))
+                    .getCollisionShape(player.getLevel(), player.getOnPos().above().relative(player.getDirection())).isEmpty();
+            boolean solidGround = !player.getLevel().getBlockState(player.getOnPos().relative(player.getDirection()))
+                    .getCollisionShape(player.getLevel(), player.getOnPos().relative(player.getDirection())).isEmpty();
+            if (faceSolid && kneeEmpty && solidGround && player.getPose() != Pose.SWIMMING) {
                 player.setPos(player.getDirection().getStepX() / 2f + player.getX(), player.getY(), player.getDirection().getStepZ() / 2f + player.getZ());
                 player.setSwimming(true);
                 player.setPose(Pose.SWIMMING);
